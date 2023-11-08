48bb4e6b3dd599c8c19a0d111fb91d907b23f2e5
Armando Almanza <armando.s.almanza@gmail.com>
2023-11-08 09:54:19 -0600

    ya guarda examenes por topics

--- server/examns/views.py
+++ server/examns/views.py
@@ -2,6 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
+from uuid import uuid4
+import random
+
 from examns.models import Question_Bank, General_Exam
 from .serializers import Question_BankSerializer, General_Exam_Serializer
 
@@ -19,41 +23,34 @@ class Question_Bank_Register(APIView):
             print(request.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+    
 class General_Exam_Register(APIView):
     def get(self, request, *args, **kwargs):
-        questions = Question_Bank.objects.filter(topic=kwargs.get("topic"))
-        serializaers = Question_BankSerializer(questions, many=True)
+        questions = General_Exam.objects.filter(topic=kwargs.get("topic"))
+        serializaers = General_Exam_Serializer(questions, many=True)
         return Response(serializaers.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
-        question_bank = Question_Bank.objects.filter(
-            topic=kwargs.get("topic"))
+        question_bank = Question_Bank.objects.filter(topic=kwargs.get("topic"))
 
-        topic_exam = Question_Bank.generate_topic_exam(
-            topic=kwargs.get("topic"),
-            questions=question_bank[0].questions,
-            user=request.data.get("user"),
-            num_questions=40)
+        # Crear una lista de diccionarios en lugar de instancias de QuestionElement
+        topic_exam = [
+            {
+                "correct_answer": question.correct_answer,
+                "question": question.question,
+                "options": question.options
+            }
+            for question in random.sample(question_bank[0].questions, 30)
+        ]
 
-        serializer = General_Exam_Serializer(data=topic_exam)
+        serializer = General_Exam_Serializer(data={
+            "exam_id": str(uuid4()),
+            "topic": kwargs.get("topic"),
+            "user": request.data.get("user"),
+            "questions": topic_exam
+        })
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class QuestionBankRegister(APIView):
-#     def get(self, request, *args, **kwargs):
-#         questions = Question_Bank.objects.all()
-#         serializaers = Question_BankSerializer(questions, many=True)
-#         return Response(serializaers.data, status=status.HTTP_200_OK)
-
-#     def post(self, request, *args, **kwargs):
-#         serializer = Question_BankSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             print(request.data)
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
