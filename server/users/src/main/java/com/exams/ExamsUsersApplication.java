8b1851bb3c8604711dcd9e90ec93a8a5c9718201
Armando Almanza <armando.s.almanza@gmail.com>
2023-11-10 19:35:17 -0600

    add a new "project"

---
+++ server/users/src/main/java/com/exams/ExamsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsUsersApplication.class, args);
+	}
+
+}
