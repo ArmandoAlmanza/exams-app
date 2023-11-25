8232c3db47ecfb1e0259fa4274d10216c3267e62
Armando Almanza <armando.s.almanza@gmail.com>
2023-11-25 17:14:17 -0600

    cnahge the client to angular

--- client/src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-const Home = () => {
-  return (
-	<div>
-		<h1>Welcome</h1>
-	</div>
-  )
-}
-export default Home
--- server/exams/examns/serializers.py
+++ server/exams/examns/serializers.py
@@ -43,14 +43,12 @@ class Topic_Exam_Serializer(DocumentSerializer):
         questions_data = validated_data.pop('questions')
         exam = Topic_Exam.objects.create(**validated_data)
 
-        # Verificamos si el campo 'questions' es None y, de ser así, lo inicializamos como una lista vacía
         if exam.questions is None:
             exam.questions = []
 
         for question_data in questions_data:
-            # Creamos un objeto QuestionElement y lo adjuntamos al examen
             question = QuestionElement(**question_data)
             exam.questions.append(question)
 
-        exam.save()  # Guardamos el examen con las preguntas
+        exam.save() 
         return exam
