73e6420de041b8a93e8a25fcb25d7a12f484d0b1
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-04 13:46:08 -0600

    a

--- client/src/app/components/landing-page/landing-page.component.ts
+++ client/src/app/components/landing-page/landing-page.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
 })
 export class LandingPageComponent {
-  logged = false;
-  helloWorld() {
-    alert('hello world');
-  }
 }
--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  logged = false;
+  helloWorld() {
+    alert('hello world');
+  }
 }
