8812931965cfa60c67950b9172f0785457507e42
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-04 13:16:50 -0600

    add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'auth', component: LoginPageComponent },
+];
--- client/src/app/components/landing-page/landing-page.component.ts
+++ client/src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './landing-page.component.html',
 })
 export class LandingPageComponent {
   logged = false;
+  helloWorld() {
+    alert('hello world');
+  }
 }
