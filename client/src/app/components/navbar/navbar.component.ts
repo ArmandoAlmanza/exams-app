be7dbf29b99d58861ed85ebaef5352d9476a6a33
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-06 11:42:01 -0600

    add a for statement for links

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faBook, faAtlas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
@@ -20,6 +20,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 export class NavbarComponent {
   faHome = faHome;
   logged = false;
+  links = [
+    { href: '/', title: 'Home', icon: faHome },
+    { href: '/general-exam', title: 'General Exam', icon: faBook },
+    { href: '/topic-exam', title: 'Topic Exam', icon: faAtlas },
+  ];
   helloWorld() {
     alert('hello world');
   }
