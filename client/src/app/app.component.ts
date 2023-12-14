ef01f02424d6b09b4cfffed74568974b67fef778
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-14 11:07:54 -0600

    aaaaaaa no jalan las animaciones

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,27 +3,28 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { Router, NavigationEnd } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
-  templateUrl: './app.component.html',
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet, NavbarComponent, BrowserAnimationsModule],
+	templateUrl: './app.component.html',
 })
 export class AppComponent {
-  showNavbar = true;
+	showNavbar = true;
 
-  constructor(private router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.showNavbar = this.shouldShowNavbar(event.url);
-      }
-    });
-  }
+	constructor(private router: Router) {
+		this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				this.showNavbar = this.shouldShowNavbar(event.url);
+			}
+		});
+	}
 
-  private shouldShowNavbar(url: string): boolean {
-    const excludedRoutes = ['/', '/auth'];
+	private shouldShowNavbar(url: string): boolean {
+		const excludedRoutes = ['/', '/auth'];
 
-    return !excludedRoutes.includes(url);
-  }
+		return !excludedRoutes.includes(url);
+	}
 }
--- client/src/app/pages/login-page/login-page.component.ts
+++ client/src/app/pages/login-page/login-page.component.ts
@@ -2,17 +2,45 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
 import { RegisterFormComponent } from '../../components/register-form/register-form.component';
+import {
+	animate,
+	state,
+	style,
+	transition,
+	trigger,
+} from '@angular/animations';
 
 @Component({
 	selector: 'app-login-page',
 	standalone: true,
 	imports: [CommonModule, LoginFormComponent, RegisterFormComponent],
 	templateUrl: './login-page.component.html',
+	animations: [
+		trigger('openClose', [
+			state(
+				'login',
+				style({
+					// Estilos para el estado de login
+					opacity: 1,
+					// ... otros estilos ...
+				})
+			),
+			state(
+				'register',
+				style({
+					// Estilos para el estado de register
+					opacity: 1,
+					// ... otros estilos ...
+				})
+			),
+			transition('login <=> register', [animate('0.5s')]),
+		]),
+	],
 })
 export class LoginPageComponent {
-	showLoginForm = true;
+	formState: 'login' | 'register' = 'login';
 
-	toggleRegister() {
-		this.showLoginForm = !this.showLoginForm;
+	toggleForm() {
+		this.formState = this.formState === 'login' ? 'register' : 'login';
 	}
 }
