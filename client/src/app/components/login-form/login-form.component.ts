97aabdc86afd403dbc1a92627d29ece5b46d4c44
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-18 09:49:17 -0600

    make forms work

--- client/src/app/components/login-form/login-form.component.ts
+++ client/src/app/components/login-form/login-form.component.ts
@@ -2,23 +2,20 @@ import { Component, EventEmitter, Output, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-login-form',
 	standalone: true,
-	imports: [CommonModule, FontAwesomeModule],
+	imports: [CommonModule, FontAwesomeModule, ReactiveFormsModule],
 	templateUrl: './login-form.component.html',
 })
 export class LoginFormComponent {
 	faEnvelope = faEnvelope;
 	faLock = faLock;
 
-	userEmail = '';
-
-	handleInput(value: string) {
-		this.userEmail = value;
-		console.log(this.userEmail);
-	}
+	email = new FormControl('');
+	password = new FormControl('');
 
 	login(event: Event) {
 		event.preventDefault();
