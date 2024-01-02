18eed6583850903514ec74f74e6d12e5635800b8
Armando Almanza <armando.s.almanza@gmail.com>
2024-01-02 09:13:13 -0600

    change the routes

--- client/src/pages/Exams.tsx
+++ client/src/pages/Exams.tsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const Exams = () => {
     return (
-        <div>
-            <h1>Here goes the exams</h1>
-        </div>
+        <main className="flex flex-col content-center md:flex-row md:h-screen md:w-full">
+            <Header />
+            <div className="md:flex-1 md:overflow-y-scroll">
+                <Outlet />
+            </div>
+        </main>
     );
 };
 
--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
-import LandingPage from "../pages/LandingPage";
 import ErrorPage from "../pages/ErrorPage";
 import Exams from "../pages/Exams";
 import Profile from "../pages/Profile";
@@ -11,25 +10,21 @@ const router = createBrowserRouter([
         path: "/",
         element: <App />,
         errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "/",
-                element: <LandingPage />,
-            },
-            {
-                path: "/exams",
-                element: <Exams />,
-            },
-            {
-                path: "/profile",
-                element: <Profile />,
-            },
-        ],
     },
     {
         path: "/auth",
         element: <Auth />,
         errorElement: <ErrorPage />,
+    },
+    {
+        path: "/exams",
+        element: <Exams />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/profile",
+        errorElement: <ErrorPage />,
+        element: <Profile />,
     },
 ]);
 
