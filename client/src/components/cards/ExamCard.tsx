24209974398685f9dbe9c8376c13ad1df37a5b7a
Armando Almanza <armando.s.almanza@gmail.com>
2024-01-02 13:50:08 -0600

    wey ya no recuerdo, muchas cosassss

--- client/src/components/cards/ExamCard.tsx
+++ client/src/components/cards/ExamCard.tsx
@@ -6,20 +6,20 @@ interface ExamProps {
 const ExamCard = ({ topic, status }: ExamProps) => {
     return (
         <article
-            className={`${topic.toLowerCase()} relative p-6 h-96 w-80 md:w-full bg-no-repeat ${
+            className={`${topic.toLowerCase()} overflow-hidden relative p-6 h-96 w-80 md:w-full bg-center bg-no-repeat ${
                 topic === "General" ? "order-1" : ""
             }`}
         >
-            <div className=" w-full z-10 p-6">
-                <h1 className="font-second font-semibold px-10 py-1 b_alt absolute bottom-3 right-10 text-white text-3xl ">
+            <div className="w-full p-6 absolute h-full ">
+                <h1 className="font-second font-semibold px-10 py-1 b_alt absolute bottom-10 right-10 text-white text-3xl ">
                     {topic}
                 </h1>
                 <p
-                    className={`bg-black px-3 py-1 text-white text-xl font-bold absolute top-3 left-10 ${status.toLowerCase()}`}
+                    className={`bg-black px-3 py-1 text-white text-xl font-bold absolute top-0 left-0 ${status.toLowerCase()}`}
                 >
                     {status}
                 </p>
-                <button className="absolute top-14 left-10 bg-black text-white px-3 py-2 mx-auto block w-36 transition-colors hover:bg-violet-400 hover:text-black duration-500 ">
+                <button className="absolute top-11 left-0 bg-black text-white px-3 py-2 mx-auto block w-36 transition-colors hover:bg-violet-400 hover:text-black duration-500 ">
                     Continue
                 </button>
             </div>
