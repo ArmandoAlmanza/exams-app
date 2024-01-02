acd7bb13718b787af6c55ba5a7b80312df467479
Armando Almanza <armando.s.almanza@gmail.com>
2024-01-02 13:25:08 -0600

    add bg images

--- client/src/components/cards/ExamCard.tsx
+++ client/src/components/cards/ExamCard.tsx
@@ -6,7 +6,7 @@ interface ExamProps {
 const ExamCard = ({ topic, status }: ExamProps) => {
     return (
         <article
-            className={`relative bg-[url(/${topic.toLowerCase()}.jpg)] p-6 h-96 w-80 md:w-full bg-no-repeat ${
+            className={`${topic.toLowerCase()} relative p-6 h-96 w-80 md:w-full bg-no-repeat ${
                 topic === "General" ? "order-1" : ""
             }`}
         >
@@ -14,7 +14,9 @@ const ExamCard = ({ topic, status }: ExamProps) => {
                 <h1 className="font-second font-semibold px-10 py-1 b_alt absolute bottom-3 right-10 text-white text-3xl ">
                     {topic}
                 </h1>
-                <p className="bg-black px-3 py-1 text-white text-xl font-bold absolute top-3 left-10">
+                <p
+                    className={`bg-black px-3 py-1 text-white text-xl font-bold absolute top-3 left-10 ${status.toLowerCase()}`}
+                >
                     {status}
                 </p>
                 <button className="absolute top-14 left-10 bg-black text-white px-3 py-2 mx-auto block w-36 transition-colors hover:bg-violet-400 hover:text-black duration-500 ">
