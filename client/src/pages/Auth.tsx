09ea23a9064af6f21cbf533863d13127ce09ef63
Armando Almanza <armando.s.almanza@gmail.com>
2024-01-02 09:46:40 -0600

    add styles for the bg

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -10,12 +10,14 @@ const Auth = () => {
     };
 
     return (
-        <div className="absolute  h-screen w-screen bg-no-repeat bg-center grid place-items-center bg-white">
-            <div className="relative top-0 left-0 border border-black mx-auto my-auto max-w-4xl p-8 m-8 flex flex-col gap-5">
+        <div className="absolute h-screen w-screen bg-no-repeat bg-center grid place-items-center bg-opacity-5 bg-[url(/authbg.jpg)] bg-cover">
+            <div className="relative top-0 left-0 border border-black mx-auto my-auto max-w-4xl p-8 m-8 flex flex-col gap-5 bg-white md:w-full md:h-max ">
                 <div className="b">
-                    <h1 className="text-xl font-bold text-center">Logn In / Sign Up</h1>
+                    <h1 className="text-xl font-bold text-center">
+                        Logn In / Sign Up
+                    </h1>
                 </div>
-                <div className="">
+                <div className="h-full w-full">
                     {state === "login" ? (
                         <LoginForm state={state} setState={handdleLogin} />
                     ) : (
