44898edf67a94bb07a2ced7fcaa859d06349b670
Armando Almanza <armando.s.almanza@gmail.com>
2024-01-02 10:49:51 -0600

    add mock data for exams

---
+++ client/src/interfaces/mock.ts
@@ -0,0 +1,18 @@
+export const exams = [
+    {
+        topic: "History",
+        questions: 120,
+    },
+    {
+        topic: "Biology",
+        questions: 120,
+    },
+    {
+        topic: "Chemestry",
+        questions: 80,
+    },
+    {
+        topic: "General",
+        questions: 120,
+    },
+];
